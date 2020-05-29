cb7212a1f2006bb8a82e5450b15cb48e083d208b
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-05-29 20:22:56 +0530

    implement APi with embedded h2 database

---
+++ src/main/java/com/dhana/tutorials/springboot/EmployeeController.java
@@ -0,0 +1,51 @@
+package com.dhana.tutorials.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController
+{
+    @Autowired
+    EmployeeService service;
+ 
+    @GetMapping
+    public ResponseEntity<List<EmployeeEntity>> getAllEmployees() {
+        List<EmployeeEntity> list = service.getAllEmployees();
+ 
+        return new ResponseEntity<List<EmployeeEntity>>(list, HttpStatus.OK);
+    }
+ 
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeEntity> getEmployeeById(@PathVariable("id") Long id)
+                                                    throws RecordNotFoundException {
+        EmployeeEntity entity = service.getEmployeeById(id);
+ 
+        return new ResponseEntity<EmployeeEntity>(entity, HttpStatus.OK);
+    }
+ 
+    @PostMapping
+    public ResponseEntity<EmployeeEntity> createOrUpdateEmployee(EmployeeEntity employee)
+                                                    throws RecordNotFoundException {
+        EmployeeEntity updated = service.createOrUpdateEmployee(employee);
+        return new ResponseEntity<EmployeeEntity>(updated, HttpStatus.OK);
+    }
+ 
+    @DeleteMapping("/{id}")
+    public HttpStatus deleteEmployeeById(@PathVariable("id") Long id)
+                                                    throws RecordNotFoundException {
+        service.deleteEmployeeById(id);
+        return HttpStatus.FORBIDDEN;
+    }
+ 
+}
